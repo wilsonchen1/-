bd08bf7f6333fd103de884bce905ecf1bb8938e0
chenyihe <645695312@qq.com>
2022-11-03 11:43:22 +0800

    周四中午版本

--- week05/src/main.tsx
+++ week05/src/main.tsx
@@ -1,6 +1,6 @@
 import "./styles/main.css";
 import "./styles/iconfont.css";
-import React, { useMemo, useState } from "react";
+import React, {useState } from "react";
 import ReactDOM from "react-dom/client";
 import TodoList, { todos } from "./todolist";
 import { todos_active,myLocalStorage } from "./todolist";
@@ -10,11 +10,11 @@ import dayjs from "dayjs";
 
 function App() {
 
-  const [Value,setValue]=useState('');
+  const [Value,setValue]=useState('');//给value设置状态，这样改动的时候重新渲染
   const inputRef=React.useRef(null);
   const getIptValue=(event: { target: { value:any; }; })=>{
     setValue(event.target.value);
-    return event.target.value;
+    return event.target.value;//获取ipt的值
   }
   return (
     <>
@@ -41,8 +41,12 @@ function App() {
                 mtime: dayjs().toString(),
                 finished: false,
               };
-              todos_active.unshift(item);//把新加入的数据放到数组的第一个位置（即按时间倒序）
-              myLocalStorage();//拼数组并存储本地
+              todos.unshift(item);//把新加入的数据放到数组的第一个位置（即按时间倒序）
+              
+              myLocalStorage(todos);//拼数组并存储本地
+              if(todos.length===1)location.reload();
+              //为了阻止第一次输入时没有办法触发渲染，
+              //我选择第一次输入时刷新一次页面
               setValue('');//清空input
             }}}
           ></input>
--- week05/src/todolist.tsx
+++ week05/src/todolist.tsx
@@ -3,40 +3,62 @@ import React, { useEffect, useState } from "react";
 import { Todo } from "./types";
 import "./styles/iconfont.css";
 import dayjs from "dayjs";
-import { v4 as uuidv4 } from "uuid";
 
-const todos: Todo[] = JSON.parse(localStorage.getItem("_todos"));
- const todos_active: Todo[] = [];
+let todos: Todo[] = JSON.parse(localStorage.getItem("_todos"));
+const todos_active: Todo[] = [];
 const todos_finished: Todo[] = [];
-function myLocalStorage() {
+function myLocalStorage(a: Todo[]) {
   //把数据存储到本地,并且维护这个数组
-  //把已做和未做数组拼在一起存储。
-  localStorage.setItem("_todos", JSON.stringify(todos));
+  //把已做和未做数组拼在一起存储
+  localStorage.setItem("_todos", JSON.stringify(a));
 }
-export {todos_active,todos}
-export {myLocalStorage}
+export { todos_active, todos };
+export { myLocalStorage };
 export default function TodoList() {
   const [visibleTodos, setVisibleTodos] = useState<Todo[]>(todos);
 
-
   function Item(props: Todo) {
+    // function Change(
+    //   // arr_finished: Todo[],
+    //   // arr_active: Todo[],
+    //   ischange: boolean,
+    //   item: Todo,
+    //   index: number
+    // ) {
+    //   if (ischange === true) {
+    //     todos_finished.unshift(item);
+    //     todos_active.splice(index, 1);
+    //     todos = todos_active.concat(todos_finished);
+    //   } else {
+    //     todos_active.unshift(item);
+    //     todos_finished.splice(index, 1);
+    //     todos = todos_active.concat(todos_finished);
+    //   }
+    // }
+
     const checked = (id: string) => {
       //修改finished的状态并存入本地
       setVisibleTodos(
         //返回需改的数组，修改状态重新渲染
-        todos.map((todo: Todo) => {
+        todos.map((todo: Todo, index: number) => {
           if (todo.id === id) {
             todo.finished = !todo.finished;
             todo.mtime = dayjs().toString(); //每次点击都要更新时间
-            // myLocalStorage(todos);
+            myLocalStorage(todos);
           }
           return { ...todo };
         })
       );
     };
     const myDelete = (id: string) => {
-      // let new_todos=todos.filter((todo) =>{ todo.id !== id;myLocalStorage(new_todos)})
-      // setVisibleTodos(new_todos)
+      // for(let i=0;i<todos.length;i++){
+      //   if(todos[i].id===id)
+      //   todos.splice(i,1);
+      // }
+      // setVisibleTodos(todos.filter((todo) => todo.id !== id));
+      todos = todos.filter((todo) => todo.id !== id);
+      setVisibleTodos(todos)
+      myLocalStorage(todos);
     };
     return (
       //这里返回的是每一条小的条目，单独拆分成一个组件
